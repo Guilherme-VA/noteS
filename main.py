15db5d373e3d94408671722e22e3ec6bd62aaf18
Guilherme Von Ahn Avila <141845048+Guilherme-VA@users.noreply.github.com>
2024-07-03 11:13:26 -0300

    Update main.py
    bug fixes & new features

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 from tkinter import colorchooser, simpledialog
-import customtkinter
-import sqlite3, os
+import sqlite3, os, time, customtkinter
+from screeninfo import get_monitors
 from pathlib import Path
 from PIL import Image, ImageDraw
 
+os.system('cls')
+
+start_time = time.time()
+
+monitors = get_monitors()
+monitor = monitors[1]
+
 class noteS_auth_app(customtkinter.CTk):
-    conn = sqlite3.connect("C:\\Users\\Mushr\\PycharmProjects\\noteS+\\database.db")
+    conn = sqlite3.connect("C:\\Users\\Mushr\\OneDrive\\Documents\\VSCODE\\Python\\noteS+\\database.db")
     cursor = conn.cursor()
 
     def __init__(self):  #FRONT-END
@@ -36,6 +43,8 @@ class noteS_auth_app(customtkinter.CTk):
                                                       command=lambda: self.register_user())
         self.registerButton.pack(side=tk.RIGHT, padx=10, pady=10)
 
+        self.labelError = customtkinter.CTkLabel(self.main_frame, text = "no user found", bg_color="red")
+
     def check_user(self):
         session_username = self.usernameEntry.get()
         session_password = self.passwordEntry.get()
@@ -64,8 +73,10 @@ class noteS_auth_app(customtkinter.CTk):
         if self.check_user():
             self.destroy()
             app = noteS_main_app(user_id)
-            app.iconbitmap("icon.ico")
             app.mainloop()
+        else:
+            self.labelError.place(x=10, y=10)
+            self.labelError.tkraise()
 
     def register_user(self):
         session_username = self.usernameEntry.get()
@@ -79,7 +90,7 @@ class noteS_auth_app(customtkinter.CTk):
                 self.cursor.execute("SELECT id FROM users WHERE username = ?", (session_username,))
                 user_id = (str(self.cursor.fetchone())).strip("()").replace(",", "")
 
-                directory = Path("C:\\Users\\Mushr\\PycharmProjects\\noteS+\\user_data\\" + user_id)
+                directory = Path("C:\\Users\\Mushr\\OneDrive\\Documents\\VSCODE\\Python\\noteS+\\user_data\\" + user_id)
 
                 if not directory.exists():
                     directory.mkdir(parents=True, exist_ok=True)
@@ -93,16 +104,15 @@ class noteS_auth_app(customtkinter.CTk):
         else:
             print("register_user() > input password")
 
-
 class noteS_main_app(customtkinter.CTk):
-    conn = sqlite3.connect("C:\\Users\\Mushr\\PycharmProjects\\noteS+\\database.db")
+    conn = sqlite3.connect("C:\\Users\\Mushr\\OneDrive\\Documents\\VSCODE\\Python\\noteS+\\database.db")
     cursor = conn.cursor()
 
     def __init__(self, user_id):
         super().__init__()
         self.user_id = user_id
+        self.after(100, lambda: self.maximize_and_stay_maximized(monitor))
 
-        self.geometry('800x650')
         self.title(f"noteS+ {user_id}")
         customtkinter.set_appearance_mode("light")
         customtkinter.set_default_color_theme("blue")
@@ -110,7 +120,7 @@ class noteS_main_app(customtkinter.CTk):
         self.main_frame = customtkinter.CTkFrame(self)
         self.main_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-        self.image = Image.new("RGB", (800, 600), "white")
+        self.image = Image.new("RGB", (monitor.width, monitor.height), "white")
         self.draw = ImageDraw.Draw(self.image)
 
         self.canvas = customtkinter.CTkCanvas(self.main_frame, bg = "white")
@@ -122,7 +132,8 @@ class noteS_main_app(customtkinter.CTk):
         self.add_lines()
 
         self.canvas.bind("<B1-Motion>", self.paint)
-
+        self.canvas.bind("<B3-Motion>", self.erase_area)
+        self.canvas.bind("<ButtonRelease-3>", self.add_lines_release)
 
     # MENU BUTTONS
 
@@ -142,39 +153,70 @@ class noteS_main_app(customtkinter.CTk):
                                                    command=lambda: self.save())
         self.save_button.pack(side=tk.LEFT, padx=10, pady=10)
 
+    def add_lines_release(self, event):
+        self.add_lines()
+
+    def erase_area(self, event):
+        user_color =  self.brush_color
+        self.brush_color = "#FFFFFF"
+        
+        x1, y1 = (event.x - self.brush_size), (event.y - self.brush_size)
+        x2, y2 = (event.x + self.brush_size), (event.y + self.brush_size)
+        self.canvas.create_oval(x1, y1, x2, y2, fill=self.brush_color, outline=self.brush_color)
+        self.draw.ellipse([x1,y1,x2,y2], fill=self.brush_color, outline=self.brush_color)
+
+        self.brush_color = user_color
+        
     def add_lines(self):
         line_spacing = 50
-        for i in range(0, 600, line_spacing):
-            self.canvas.create_line(0, i, 1200, i, fill="lightgray")
-            self.draw.line((0, i, 12800, i), fill="lightgray")
-
+        for i in range(0, 1920, line_spacing):
+            self.canvas.create_line(1, i, 1920, i, fill="lightgray")
+            self.draw.line((1, i, 1920, i), fill="lightgray")
 
     def paint(self, event):
         x1, y1 = (event.x - self.brush_size), (event.y - self.brush_size)
         x2, y2 = (event.x + self.brush_size), (event.y + self.brush_size)
         self.canvas.create_oval(x1, y1, x2, y2, fill=self.brush_color, outline=self.brush_color)
         self.draw.ellipse([x1,y1,x2,y2], fill=self.brush_color, outline=self.brush_color)
+
     def choose_color(self):
         self.brush_color = colorchooser.askcolor(color=self.brush_color)[1]
+
     def choose_size(self):
-        input_dialog = simpledialog.askinteger("choose size","brush size from 1-10", minvalue=1, maxvalue=10)
+        input_dialog = simpledialog.askinteger("choose size","brush size from 1-30", minvalue=1, maxvalue=30)
         if input_dialog:
             self.brush_size = input_dialog
+
     def clear(self):
         self.canvas.delete("all")
-        self.image = Image.new("RGB", (800, 600), "white")
+        self.image = Image.new("RGB", (1920, 1080), "white")
         self.draw = ImageDraw.Draw(self.image)
 
         self.add_lines()
+
     def save(self):
         input_dialog = customtkinter.CTkInputDialog(title="file name", text="input a name for the file")
-        file_path = f"C:\\Users\\Mushr\PycharmProjects\\noteS+\\user_data\\{self.user_id}\\{input_dialog.get_input()}.png"
+
+        file_path = f"C:\\Users\\Mushr\\OneDrive\\Documents\\VSCODE\\Python\\noteS+\\user_data\\{self.user_id}\\{input_dialog.get_input()}.png"
+
         self.image.save(file_path)
         print(f"save() >image save {file_path}")
+    
+    def maximize_and_stay_maximized(self, monitor):
+        self.geometry(f"{monitor.width}x{monitor.height}")
+        self.update_idletasks()
+        self.state("zoomed")
+
+#runtime
+end_time = time.time()
+elapsed = end_time - start_time
+print(f"runtime: {elapsed}")
+#runtime
 
 
-app = noteS_auth_app()
-app.iconbitmap("icon.ico")
-app.mainloop()
+if __name__ == "__main__":
+    
+    app = noteS_auth_app()
+    app.mainloop()
 
 print(0)
